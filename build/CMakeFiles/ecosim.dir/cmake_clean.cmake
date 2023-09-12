d7efa78364cac31cf03178f361aa763bc35c6143
lucasibrahimss <142946220+lucasibrahimss@users.noreply.github.com>
2023-09-12 23:39:24 +0000

    funcionando criacao do tabuleiro

---
+++ build/CMakeFiles/ecosim.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ecosim.dir/src/main.cpp.o"
+  "CMakeFiles/ecosim.dir/src/main.cpp.o.d"
+  "ecosim"
+  "ecosim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ecosim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/ecosim.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ecosim.
+# This may be replaced when dependencies are built.
